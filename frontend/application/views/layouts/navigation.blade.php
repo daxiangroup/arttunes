405969d86c68b2ce5e9cf463743ea3600d089fd6
Tyler Schwartz <tylers@nationalfibre.net>
2012-10-30 09:54:57 -0400

    added login link when not logged in

--- frontend/application/views/layouts/navigation.blade.php
+++ frontend/application/views/layouts/navigation.blade.php
@@ -1,7 +1,11 @@
 <div id="cntr-navigation">
     <ul>
+        @if (Auth::check())
         <li>{{ HTML::link('/dashboard', 'Dashboard') }}</li>
         <li>{{ HTML::link('/profile', 'Profile') }}</li>
         <li>{{ HTML::link('/logout', 'Logout') }}</li>
+        @else
+        <li>{{ HTML::link('/login', 'Login') }}</li>
+        @endif
     </ul>
 </div>
