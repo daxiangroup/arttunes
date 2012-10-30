4dd21b7014915d6758dc93b685a232cb17d5eddf
Tyler Schwartz <ts@daxiangroup.com>
2012-10-29 22:55:42 -0400

    adding navigation partial for master layout

---
+++ frontend/application/views/layouts/navigation.blade.php
@@ -0,0 +1,7 @@
+<div id="cntr-navigation">
+    <ul>
+        <li>{{ HTML::link('/dashboard', 'Dashboard') }}</li>
+        <li>{{ HTML::link('/profile', 'Profile') }}</li>
+        <li>{{ HTML::link('/logout', 'Logout') }}</li>
+    </ul>
+</div>
