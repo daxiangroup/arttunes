a822a672d3ef11518104df8f79ce3f67d5a1c1c3
Tyler Schwartz <tylers@nationalfibre.net>
2012-11-01 23:37:12 -0400

    galleries link in the main navigation

--- application/application/views/layouts/navigation.blade.php
+++ application/application/views/layouts/navigation.blade.php
@@ -2,7 +2,8 @@
     <ul>
         @if (Auth::check())
         <li>{{ HTML::link('/dashboard', 'Dashboard') }}</li>
-        <li>{{ HTML::link('/profile', 'Profile') }}</li>
+        <li>{{ HTML::link('/profile/'.Auth::user()->username, 'Profile') }}</li>
+        <li>{{ HTML::link('/galleries', 'Galleries') }}</li>
         <li>{{ HTML::link('/logout', 'Logout') }}</li>
         @else
         <li>{{ HTML::link('/login', 'Login') }}</li>
