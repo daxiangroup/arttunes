6d918aacbc4a05cb0e0f0c4ccb81f5c06a3d2c10
Tyler Schwartz <tylers@nationalfibre.net>
2012-11-03 06:15:59 -0400

    profile bundle routing changes

--- bundles/application/profile/routes.php
+++ bundles/application/profile/routes.php
@@ -2,10 +2,22 @@
 use \Profile\Services\Profile AS ProfileService;
 
 Route::group(array('before'=>'auth'), function() {
+    // Temporary route, this will likely be rewritten. For now, it redirects to
+    // the logged in user's galleries.
+    Route::get('/profile', function() {
+        return Redirect::to('/profile/'.Auth::user()->username);
+    });
+
     Route::get('/profile/(:any)', function() {
-        die(Bundle::name());
+        $success = ProfileService::get_id_from_username(URI::segment(2));
+
+        if (!$success['success']) {
+            return Redirect::to('/profile/'.Auth::user()->username);
+        }
+
+        $id = $success['payload'];
 
         return View::make('profile::profile')
-            ->with('profile', ProfileService::get(Auth::user()->id));
+            ->with('profile', ProfileService::get($id));
     });
 });
